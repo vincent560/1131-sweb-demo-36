95a37075cf0d03b7580b49528a94a59da3d528a1
vincent560 <vincent69021@gmail.com>
2024-10-25 13:52:06 +0800

    ### W07-P3: Connect your Github repo to Vercel and have a root home page to class demo navigation

--- demo/w07_36.js
+++ demo/w07_36.js
@@ -1,1 +1,17 @@
-alert('Hello')
+const p =document.querySelector('.show-classdemo')
+console.log('show-classdemo', p)
+
+function showDemo(item) {
+   switch(item) {
+    case 'w02_intro_36':
+    p.innerHTML=`<iframe src='./w02-intro-36/index.html' width="100%" height="100%" />`
+
+    break
+    case 'w03_card_36':
+    p.innerHTML=`<iframe src='./w03_card_36/index.html' width="100%" height="100%" />`
+    break
+    case 'w06_blog_36':
+    p.innerHTML=`<iframe src='./w06_blog_36/index.html' width="100%" height="100%" />`
+    break
+   }
+}
